c9ebba0b048fe62c28e6cb62b9e13532caf4a1e9
Supriya Gupta <supu4106@gmail.com>
2023-02-17 14:11:50 +0530

    added functioning to the body

---
+++ index.js
@@ -0,0 +1,68 @@
+
+var prt = document.getElementById("container")
+var ch1 = document.getElementById("redbox")
+redbox.onclick = () => {
+    prt.removeChild(ch1);
+};
+
+var prt = document.getElementById("container")
+var ch2 = document.getElementById("greenbox")
+greenbox.onclick = () => {
+    prt.removeChild(ch2);
+};
+
+var prt = document.getElementById("container")
+var ch3 = document.getElementById("yellowbox")
+yellowbox.onclick = () => {
+    prt.removeChild(ch3);
+};
+
+var prt = document.getElementById("container")
+var ch4 = document.getElementById("bluebox")
+bluebox.onclick = () => {
+    prt.removeChild(ch4);
+};
+
+var prt = document.getElementById("container")
+var ch5 = document.getElementById("blackbox")
+blackbox.onclick = () => {
+    prt.removeChild(ch5);
+};
+
+var prt = document.getElementById("container")
+var ch6 = document.getElementById("purplebox")
+purplebox.onclick = () => {
+    prt.removeChild(ch6);
+};
+
+var prt = document.getElementById("container")
+var ch7 = document.getElementById("pinkbox")
+pinkbox.onclick = () => {
+    prt.removeChild(ch7);
+};
+
+var prt = document.getElementById("container")
+var ch8 = document.getElementById("orangebox")
+orangebox.onclick = () => {
+    prt.removeChild(ch8);
+};
+
+var prt = document.getElementById("container")
+var ch9 = document.getElementById("skybluebox")
+skybluebox.onclick = () => {
+    prt.removeChild(ch9);
+};
+
+var prt = document.getElementById("container")
+var ch10 = document.getElementById("greybox")
+greybox.onclick = () => {
+    prt.removeChild(ch10);
+};
+
+
+
+
+
+
+
+
